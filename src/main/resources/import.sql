b035f91154daea237f20b9d1cccc6fcc7eea8804
Ignacio <ignacio.moralesyanez@externos-cl.cencosud.com>
2022-09-30 12:56:52 -0300

    add mysql config

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-2', 'Ignacio', 'Morales', 'imorales@gmail.com', '2022-09-30');
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-3', 'Ignacio2', 'Morales2', 'imorales2@gmail.com', '2022-09-29');
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-4', 'Ignacio3', 'Morales3', 'imorales3@gmail.com', '2022-09-28');
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-5', 'Ignacio4', 'Morales4', 'imorales4@gmail.com', '2022-09-27');
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-6', 'Ignacio5', 'Morales5', 'imorales5@gmail.com', '2022-09-26');
-INSERT INTO clients (dni, name, last_name, email, create_at) VALUES ('18468673-7', 'Ignacio6', 'Morales6', 'imorales6@gmail.com', '2022-09-25');
